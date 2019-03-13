fd61f450ebed46318320f996c5da48fee7b1f615
satyajitvure <satyajith.464@gmail.com>
2019-03-12 20:20:45 -0600

    Test commit

---
+++ app/src/main/java/com/procore/prdiffs/model/DiffDisplay.java
@@ -0,0 +1,38 @@
+package com.procore.prdiffs.model;
+
+public class DiffDisplay {
+
+    public enum DiffType {
+        DIFF,
+        HUNK_HEADER,
+        LINE
+    }
+
+    private Object diffObj;
+    private DiffType diffType;
+    private String lineNum;
+
+    public Object getDiffObj() {
+        return diffObj;
+    }
+
+    public void setDiffObj(Object diffObj) {
+        this.diffObj = diffObj;
+    }
+
+    public DiffType getDiffType() {
+        return diffType;
+    }
+
+    public void setDiffType(DiffType diffType) {
+        this.diffType = diffType;
+    }
+
+    public String getLineNum() {
+        return lineNum;
+    }
+
+    public void setLineNum(String lineNum) {
+        this.lineNum = lineNum;
+    }
+}
